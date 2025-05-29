dcc4a1baee5027dbcbce6a482ae119b2b9de039e
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-05-29 11:23:01 +0530

    updated

--- day1/identifierdemo.py
+++ day1/identifierdemo.py
@@ -4,18 +4,23 @@ Author: Parameswari
 Date: 2023-10-01
 Description: This script reads employee data from a user or it will generate date using faker.
 """
-
+from datetime import datetime
 employeeId=input("Enter employee id: ")
 name=input("Enter employee name: ")
 emailId=input("Enter employee email: ")
 contactNumber=input("Enter employee contact number: ")
 designation=input("Enter employee designation: ")
+dob_input=input("Enter employee date of birth (dd-mm-yyyy): ")
+dob=datetime.strptime(dob_input, "%d-%m-%Y").date()
+
+
 #formatting the output
 print("Employee Id: ", employeeId)
 print("Employee Name: ", name)
 print("Employee Email: ", emailId)      
 print("Employee Contact Number: ", contactNumber)
 print("Employee Designation: ", designation)
+print("Employee Date of Birth: ", dob.strftime("%d-%m-%Y"))
 #displaying the output
 print("Employee Details:")
 print(f"Id: {employeeId}, Name: {name}, Email: {emailId}, Contact: {contactNumber}, Designation: {designation}")
