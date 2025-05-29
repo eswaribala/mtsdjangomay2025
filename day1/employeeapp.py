244da3c0d872660a9801cf4b0b12b592a0eba63f
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-05-29 16:48:08 +0530

    updated

--- day1/employeeapp.py
+++ day1/employeeapp.py
@@ -2,5 +2,5 @@ from models.employee import Employee
 #create objects of Employee class
 
 employee=Employee(1, "John Doe", "Software Engineer", 60000,"param@gmail.com",9952032862)
-employee.__id=8468
+employee.set_id(4677)
 print(employee.get_details())
--- day1/models/employee.py
+++ day1/models/employee.py
@@ -7,7 +7,32 @@ class Employee:
         self.__email = email
         self.__contactNo = contactNo
 
-    def get_details(self):
+    def get_email(self):
+        return self.__email
+    def get_contact_no(self):
+        return self.__contactNo
+    def set_email(self, email):
+        self.__email = email    
+    def set_contact_no(self, contactNo):    
+        self.__contactNo = contactNo
+    def set_name(self, name):
+        self.__name = name
+    def set_designation(self, designation):
+        self.__designation = designation     
+    def set_salary(self, salary):
+        self.__salary = salary      
+    def set_id(self, id):
+        self.__id = id  
+    def get_id(self):   
+        return self.__id
+    def get_name(self): 
+        return self.__name
+    def get_designation(self):
+        return self.__designation
+    def get_salary(self):
+        return self.__salary
+    
+    def get_details(self):  
         return f"Id:{self.__id},Name: {self.__name}, Designation: {self.__designation}, Salary: {self.__salary}"
 
     
