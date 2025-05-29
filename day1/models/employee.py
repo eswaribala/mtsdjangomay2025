25a122a57822489796988533d3f13e778ee6077d
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-05-29 16:45:00 +0530

    updated

--- day1/models/employee.py
+++ day1/models/employee.py
@@ -1,13 +1,13 @@
 class Employee:
     def __init__(self, id, name, designation, salary,email, contactNo):
-        self.id= id
-        self.name = name
-        self.designation = designation
-        self.salary = salary
-        self.email = email
-        self.contactNo = contactNo
+        self.__id= id
+        self.__name = name
+        self.__designation = designation
+        self.__salary = salary
+        self.__email = email
+        self.__contactNo = contactNo
 
     def get_details(self):
-        return f"Name: {self.name}, Designation: {self.designation}, Salary: {self.salary}"
+        return f"Id:{self.__id},Name: {self.__name}, Designation: {self.__designation}, Salary: {self.__salary}"
 
     
